f47e3602a86c8fa3fdbcb0c3966f4c06f33bae65
kusano koki <kusano@ai.cs.kobe-u.ac.jp>
2020-03-01 20:34:33 +0900

    decide loader style

---
+++ loader/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from .load import __all__
---
+++ loader/load.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+def load_csv():
+    print("load csv")
+
+
+def load_img():
+    print("load img")
+
+
+__all__ = (load_csv, load_img)
